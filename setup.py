0af923e2b5d54be55bedbbaeea7b8e9bba5664a1
LorenzoBtz <lorenzobtz@MacBook-Pro-di-Lorenzo-5.local>
2020-10-07 14:38:30 +0200

    fix install issues

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.rst", "r") as fh:
 
 setuptools.setup(
     name="django-signals-cloudevents",
-    version="0.1.0",
+    version="0.1.1",
     author="Airspot S.r.l.",
     author_email="info@airspot.tech",
     description="App to produce Cloudevents from Django model signals",
     long_description=long_description,
-    long_description_content_type="text/rst",
+    long_description_content_type="text/markdown",
     url="https://github.com/airspot-dev/django-signals-cloudevents",
-    packages=setuptools.find_packages(),
+    packages=["django_signals_cloudevents"],
     package_data={},
     include_package_data=True,
     classifiers=[
@@ -21,6 +21,8 @@ setuptools.setup(
         "Framework :: Django",
     ],
     install_requires=[
+        'requests==2.24.0',
+        'cloudevents==0.3.0'
     ],
     tests_require=[
         'django-fake-model==0.1.4',
